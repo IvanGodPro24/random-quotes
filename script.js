d4b52c8a9c88261c172c8db98f26fb58dfcafedf
IvanGodPro24 <vanyanep@gmail.com>
2026-01-28 21:14:13 +0200

    split code to the module

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-const quotes = [
-  {
-    text: "The only way to do great work is to love what you do.",
-    author: "Steve Jobs",
-  },
-  {
-    text: "Innovation distinguishes between a leader and a follower.",
-    author: "Steve Jobs",
-  },
-  {
-    text: "Your time is limited, so don't waste it living someone else's life.",
-    author: "Steve Jobs",
-  },
-  {
-    text: "The future belongs to those who believe in the beauty of their dreams.",
-    author: "Eleanor Roosevelt",
-  },
-  {
-    text: "It is during our darkest moments that we must focus to see the light.",
-    author: "Aristotle",
-  },
-];
+import { quotes } from "./quotes.js";
 
 const quoteElement = document.getElementById("quote");
 const authorElement = document.getElementById("author");
