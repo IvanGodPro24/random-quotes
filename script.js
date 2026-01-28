dedd59d7c3912bd10d8eb48d998f3534488abf1f
IvanGodPro24 <vanyanep@gmail.com>
2026-01-28 21:00:43 +0200

    refactor

--- script.js
+++ script.js
@@ -28,13 +28,14 @@ const generateBtn = document.getElementById("generate-btn");
 const generateRandomQuote = () => {
   let randomQuote;
 
-  do { 
+  do {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     randomQuote = quotes[randomIndex];
   } while (randomQuote.text === quoteElement.textContent);
 
   quoteElement.textContent = randomQuote.text;
   authorElement.textContent = `- ${randomQuote.author}`;
+  // authorElement.innerHTML = `<span>- ${randomQuote.author}</span>`;
 };
 
 generateBtn.addEventListener("click", generateRandomQuote);
