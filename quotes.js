acd07e0ec26cf7c425826c2c0f683429032df6b0
IvanGodPro24 <vanyanep@gmail.com>
2026-01-29 20:17:10 +0200

    Add favourite feature

--- quotes.js
+++ quotes.js
@@ -2,21 +2,26 @@ export const quotes = [
   {
     text: "The only way to do great work is to love what you do.",
     author: "Steve Jobs",
+    isFavourite: false,
   },
   {
     text: "Innovation distinguishes between a leader and a follower.",
     author: "Steve Jobs",
+    isFavourite: false,
   },
   {
     text: "Your time is limited, so don't waste it living someone else's life.",
     author: "Steve Jobs",
+    isFavourite: false,
   },
   {
     text: "The future belongs to those who believe in the beauty of their dreams.",
     author: "Eleanor Roosevelt",
+    isFavourite: false,
   },
   {
     text: "It is during our darkest moments that we must focus to see the light.",
     author: "Aristotle",
+    isFavourite: false,
   },
 ];
